3bb4ea22e392676c8f074c57475177f878f9db7a
multisula <multisula@gmail.com>
2023-07-30 12:14:01 +0900

    java11 4 Authentication backend integration

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,15 +1,15 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Builder
 @NoArgsConstructor
@@ -18,11 +18,11 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Todo")
 public class TodoEntity {
-  @Id
-  @GeneratedValue(generator = "system-uuid")
-  @GenericGenerator(name = "system-uuid", strategy = "uuid")
-  private String id;
-  private String userId;
-  private String title;
-  private boolean done;
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	private String id;
+	private String userId;
+	private String title;
+	private boolean done;
 }
